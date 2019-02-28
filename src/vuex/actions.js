ebdb6f573e5226820b1df7a3e394a9576ddb3f7b
Liuxs <xiao.sa@icloud.com>
2019-02-28 13:45:03 +0800

    添加 qs, 调通api请求

--- src/vuex/actions.js
+++ src/vuex/actions.js
@@ -1,6 +1,7 @@
-// 获取秒杀数据
 import Axios from 'axios';
+import qs from 'qs';
 
+// 获取秒杀数据
 export const loadSeckillsInfo = ({commit}) => {
   return new Promise((resolve, reject) => {
     const data = [
@@ -710,13 +711,20 @@ export const loadShoppingCart = ({commit}) => {
 // 添加注册用户
 export const addSignUpUser = ({commit}, data) => {
   return new Promise((resolve, reject) => {
-    const userArr = localStorage.getItem('users');
-    let users = [];
-    if (userArr) {
-      users = JSON.parse(userArr);
-    }
-    users.push(data);
-    localStorage.setItem('users', JSON.stringify(users));
+    let postData = {};
+    postData.name = data.username;
+    postData.nickname = data.username;
+    postData.password = data.password;
+    postData.apassword = data.password;
+    postData.profile = '';
+    postData = qs.stringify(postData);
+    Axios.post('http://192.144.186.149:8084/api/user/register', postData)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   });
 };
 
@@ -726,34 +734,14 @@ export const login = ({commit}, data) => {
     let postData = {};
     postData.name = data.username;
     postData.password = data.password;
-    Axios.post('http://192.144.186.149:8084/api/user/login', JSON.stringify(postData))
+    postData = qs.stringify(postData);
+    Axios.post('http://192.144.186.149:8084/api/user/login', postData)
       .then(function (response) {
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
-    // if (data.username === 'Gavin' && data.password === '123456') {
-    //   localStorage.setItem('loginInfo', JSON.stringify(data));
-    //   commit('SET_USER_LOGIN_INFO', data);
-    //   resolve(true);
-    //   return true;
-    // }
-    // const userArr = localStorage.getItem('users');
-    // console.log(userArr);
-    // if (userArr) {
-    //   const users = JSON.parse(userArr);
-    //   for (const item of users) {
-    //     if (item.username === data.username) {
-    //       localStorage.setItem('loginInfo', JSON.stringify(item));
-    //       commit('SET_USER_LOGIN_INFO', item);
-    //       resolve(true);
-    //       break;
-    //     }
-    //   }
-    // } else {
-    //   resolve(false);
-    // }
   });
 };
 
